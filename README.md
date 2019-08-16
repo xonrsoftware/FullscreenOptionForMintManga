63dab50a4a8b721e7d0cd66bfa71ca6340364586
AVX <avx@xonr.ru>
2019-08-16 12:01:17 +0300

    Init.

---
+++ README.md
@@ -0,0 +1,11 @@
+1) Установите Tampermonkey для вашего браузера:
+
+- For Chrome - https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=ru;
+
+- For Firefox -  https://addons.mozilla.org/ru/firefox/addon/tampermonkey/;
+
+- For Opera - https://addons.opera.com/ru/extensions/details/tampermonkey-beta/;
+
+- For Safari - https://safari-extensions.apple.com/details/?id=net.tampermonkey.safari-G3XV72R5TC
+
+2) Откройте ссылку скрипта: https://github.com/xonrsoftware/FullscreenOptionForMintManga/raw/master/script.user.js и нажмите "Install";
---
+++ script.user.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         Native Fullscreen Option For MintManga
+// @version      1.0
+// @author       НИНРИ (https://discord.gg/PSTM5gh)
+// @match        http://mintmanga.com/*
+// @match        https://mintmanga.com/*
+// @updateURL    https://github.com/xonrsoftware/FullscreenOptionForMintManga/raw/master/script.user.js
+// @downloadURL  https://github.com/xonrsoftware/FullscreenOptionForMintManga/raw/master/script.user.js
+// @grant        GM_addStyle
+// ==/UserScript==
+
+function switchToFullscreen() {
+    if (
+        document.fullscreenElement ||
+        document.webkitFullscreenElement ||
+        document.mozFullScreenElement ||
+        document.msFullscreenElement
+    ) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    } else {
+        const element = document.getElementById("fotocontext");
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    }
+}
+
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        for (let navbar of document.getElementsByClassName("topControl")) {
+            let near_button = navbar.getElementsByClassName("near-button")[0];
+            if (near_button !== undefined) {
+                let zNode = document.createElement('div');
+                zNode.innerHTML = `<button class="btn btn-primary" title="" id="switchToFullscreenButton" data-toggle="tooltip" data-original-title="Полноэкранный режим."><i class="fa fa-arrows-alt"></i></button>`;
+                zNode.setAttribute('id', 'switchToFullscreenDiv');
+                near_button.appendChild(zNode);
+                document.getElementById("switchToFullscreenButton").addEventListener(
+                    "click", switchToFullscreen, false
+                );
+            }
+        }
+    });
+})();
+
+GM_addStyle(`
+#switchToFullscreenDiv {
+    display: inline;
+    padding-left: 8px;
+}
+
+#fotocontext {
+    overflow: auto;
+}
+`);
